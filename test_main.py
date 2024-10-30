00cdfe288c852f94d6c6b84386e56c52b46cd500
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-10-30 11:52:08 +0100

    added test login

--- test_main.py
+++ test_main.py
@@ -22,10 +22,29 @@ def test_register():
     assert respone.json() == {"message": "user created successfuly"}
 
 
-def test_login():
+def test_login_and_protected_route():
     response = client.post("/auth/token", data={"username": "john", "password": "secret"})
     assert response.status_code == 200
     token = response.json()["access_token"]
     assert isinstance(token, str)
     assert len(token) > 0  # Check if token has some length
-    print(token)
+
+    # Step 3: Access protected route with token
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.get("/user/me",headers = headers)
+    assert response.status_code == 200
+    assert response.json()=={"username":"john"}
+
+    # Step 4: Change password
+    new_password = "newtestpass"
+    response = client.post("/auth/user/change_password", json={"new_password": new_password}, headers=headers)
+    assert response.status_code == 200
+    assert response.json()=={"message":"password updated succesfuly"}
+
+    # Step 5: Log in with new password
+    response = client.post("/auth/token",data={"username":"john","password":new_password})
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+
+
+
